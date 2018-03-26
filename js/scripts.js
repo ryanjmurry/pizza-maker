3faaa5d339022050d6869e5c18e17993d97b01d7
Ryan Murry <ryanjmurry@gmail.com>
2018-03-26 15:25:21 -0700

    add javascript and jquery to grab user input and determine the price of their pizza.

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+//Business logic
+function Pizza(size, crust, sauce, cheese, meat, nonMeat) {
+  debugger;
+  this.size = size;
+  this.crust = crust;
+  this.sauce = sauce;
+  this.cheese = cheese;
+  this.meat = meat;
+  this.nonMeat = nonMeat;
+}
+
+Pizza.prototype.Order = function() {
+  debugger;
+  var pizzaSize = this.size;
+  var pizzaCrust = this.crust;
+  var pizzaSauce = this.sauce;
+  var pizzaCheese = this.cheese.length;
+  var pizzaMeat = this.meat.length;
+  var pizzaNonMeat = this.nonMeat.length;
+
+  if (pizzaSize === "Personal") {
+    return 8+(pizzaCheese*3)+(pizzaMeat*2)+(pizzaNonMeat*1.50)
+  } else if (pizzaSize === "Small") {
+    return 10+(pizzaCheese*3)+(pizzaMeat*2)+(pizzaNonMeat*1.50)
+  } else if (pizzaSize === "Medium") {
+    return 12+(pizzaCheese*3)+(pizzaMeat*2)+(pizzaNonMeat*1.50)
+  } else if (pizzaSize === "Large") {
+    return 14+(pizzaCheese*3)+(pizzaMeat*2)+(pizzaNonMeat*1.50)
+  } else if (pizzaSize === "Jumbo") {
+    return 16+(pizzaCheese*3)+(pizzaMeat*2)+(pizzaNonMeat*1.50)
+  }
+};
+//UI logic
+$(document).ready(function() {
+  $("form#pizza-order").submit(function() {
+    event.preventDefault();
+    debugger;
+    var userCheese = [];
+    var userMeat = [];
+    var userNonMeat = [];
+    var userSize = $("#pizza-size").val();
+    var userCrust = $("#pizza-crust").val();
+    var userSauce = $("#pizza-sauce").val();
+    $("input:checkbox[name=pizza-cheese]:checked").each(function () {
+      userCheese.push($(this).val());
+    });
+    $("input:checkbox[name=meat-toppings]:checked").each(function () {
+      userMeat.push($(this).val());
+    });
+    $("input:checkbox[name=non-meat-toppings]:checked").each(function () {
+      userNonMeat.push($(this).val());
+    });
+    var userPizza = new Pizza (userSize, userCrust, userSauce, userCheese, userMeat, userNonMeat);
+    var order = userPizza.Order().toFixed(2);
+  });
+});
